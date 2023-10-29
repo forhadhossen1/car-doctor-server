ca2497e3a59023031698a116fe3e3b76419f8ede
forhadhossen1 <mr.forhad1030@gamil.com>
2023-10-30 02:08:14 +0600

    booking system save in mangodb done

--- index.js
+++ index.js
@@ -30,24 +30,35 @@ async function run() {
         await client.connect();
 
         const serviceCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings')
 
-        app.get('/services', async(req, res) => {
+        // Service related ...
+
+        app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
 
-        app.get('/services/:id', async(req, res) => {
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
 
             const options = {
                 projection: { title: 1, price: 1, service_id: 1, img: 1 },
-              };
+            };
 
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
 
+        })
+
+        //  Booking related....
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
