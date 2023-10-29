2e31c057ffd7e15da0d56866b29d1af4b36ef1b1
forhadhossen1 <mr.forhad1030@gamil.com>
2023-10-30 05:16:05 +0600

    update booking confirmation system

--- index.js
+++ index.js
@@ -54,11 +54,43 @@ async function run() {
         })
 
         //  Booking related....
+
+        app.get('/bookings', async (req, res) => {
+            console.log(req.query.email)
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
             const result = await bookingCollection.insertOne(booking);
             res.send(result);
+        })
+
+        app.patch('/bookings/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updateBooking = req.body;
+            console.log(updateBooking);
+            const updateDoc = {
+                $set: {
+                    status : updateBooking.status
+                }
+            }
+            const result = await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/bookings/:id', async(req, res)=> {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result)
         })
 
         // Send a ping to confirm a successful connection
